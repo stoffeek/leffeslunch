d6b39a8d126bb7b6ed0f7f0b32bcea1c739afcd6
arkainas <kevin.vikander@gmail.com>
2024-10-01 13:49:55 +0200

    Added database connection for sqlite

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 
 // Skapa en instans av Express
 const app = express();
 
 // Port för servern
 const PORT = 5001; 
+
+const db = new sqlite3.Database('leffes.db', (err) => {
+  if (err) {
+    console.error('Error opening database: ' + err.message);
+  } else {
+    console.log('Connected to the sqlite database');
+  }
+})
 
 // Middleware
 app.use(cors()); // Tillåter cross-origin-requests (ex. från din frontend till backend)
