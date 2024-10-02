34307a8d6990ba3d86e2f65fd0b52a0355a41f97
stoffeek <stoffeek@gmail.com>
2024-10-02 11:45:34 +0200

    routing added and home added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Header from './pages/Header'
 
 function App() {
   return(
@@ -8,7 +10,6 @@ function App() {
         <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="NoName" element={<Noname />} />  
         </Routes>
 
     
---
+++ src/pages/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/" className="link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
+
