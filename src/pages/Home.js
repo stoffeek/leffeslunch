6026f5c2a29c62c67341f7db617adc483fd17944
paufer2337 <paufer2337@gmail.com>
2024-10-02 15:46:12 +0200

    frontpage animations and client logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Home.css';
 import homelogo from './img/home_logo.png'; 
 import slogan from './img/slogan.png'; 
-
+import leffelogo from './img/leffelogo.png';
 
 function Home() {
   return (
     <div>
       <img src={homelogo} alt="Homelogo" className="fade-in homelogo" />
       <img src={slogan} alt="Slogan" className="fade-in slogan" />
+      <h2 className="fade-in subheader">CLIENTELE:</h2>
+      <img src={leffelogo} alt="Leffelogo" className="zoom leffelogo" />
     </div>
   );
 }
